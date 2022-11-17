ab6b23febcfea6599f38e689484f4216468cd5a0
Bryan Ramos <bryan.ramos@hey.com>
2022-11-17 07:15:54 -0500

    make this function private

--- asciify/helpers/map_processor.py
+++ asciify/helpers/map_processor.py
@@ -9,12 +9,12 @@ class MapProcessor:
 
     def get_char(self, code: int):
         for char, range in self.data.items():
-            if self.out_of_range(code, range):
+            if self.__out_of_range(code, range):
                 continue
             else:
                 return char
 
-    def out_of_range(self, val: int, range: list):
+    def __out_of_range(self, val: int, range: list):
         low, high = range[0], range[1]
         if val < low or val > high:
             return True
