457566db43ef61e75cd6cac08e1e27aa09d6dda4
Bryan Ramos <bryan.ramos@hey.com>
2022-11-22 13:28:49 -0500

    update argument parser data

--- asciify/__main__.py
+++ asciify/__main__.py
@@ -3,10 +3,10 @@ import app
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file_name', help="image file to process", default=None, type=str)
-    parser.add_argument('-s', '--shrink_ratio', help="shrink ratio for image", default=15, type=int)
-    parser.add_argument('-o', '--output_file', help='file to output to', default='res.txt', type=str)
-    parser.add_argument('-m', '--char_map', help="char map file to map pixel values to characters", default="character_maps/default.json", type=str)
+    parser.add_argument('-f', '--file_name', help="image file to process (required)", default=None, type=str)
+    parser.add_argument('-s', '--shrink_ratio', help="shrink ratio for image, defaults to 1", default=1, type=int)
+    parser.add_argument('-o', '--output_file', help='file to output to, defaults to res.txt', default='res.txt', type=str)
+    parser.add_argument('-m', '--char_map', help="char map file to map pixel values to characters, defaults to character_maps/default.json", default="character_maps/default.json", type=str)
     args = parser.parse_args()
     app.run(args.file_name, args.shrink_ratio, args.char_map, args.output_file)
 
